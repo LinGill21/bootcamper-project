e22d6b9a238085cc800f2d13fce55ba2c4de5d53
LindsayG <lind-gillespie@sbcglobal.net>
2021-07-22 13:15:57 -0500

    broke apart the controller to each go with a page. Got rid of redundant controller

---
+++ src/main/java/com/organization/mvcproject/MGL_Task1/controller/ReviewController.java
@@ -0,0 +1,34 @@
+package com.organization.mvcproject.MGL_Task1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.organization.mvcproject.MGL_Task1.model.Game;
+import com.organization.mvcproject.MGL_Task1.model.Review;
+import com.organization.mvcproject.MGL_Task1.service.Game_Service;
+
+@Controller
+public class ReviewController{
+	@RequestMapping(value = "/review", method = RequestMethod.GET)
+	public ModelAndView review() {
+		return new ModelAndView("review", "command", new Review());
+	}
+
+	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
+	public ModelAndView addReview(Review review, ModelMap model) {
+		if(review.getAuthor().equals("")) {
+			review.setAuthor("anonymous");
+		}
+		return new ModelAndView("result", "submittedReview", review);
+	}
+}

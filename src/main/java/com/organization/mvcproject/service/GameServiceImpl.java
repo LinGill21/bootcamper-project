f6c446a8a28b7f1a326964bafc2de39e9dbebeaa
LindsayG <lind-gillespie@sbcglobal.net>
2021-07-29 08:04:03 -0500

    updated GameService to use game interface

--- src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
+++ src/main/java/com/organization/mvcproject/service/GameServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.organization.mvcproject.dao.GameDaoImpl;
 import com.organization.mvcproject.api.service.GameService;
-import com.organization.mvcproject.model.Game;
+import com.organization.mvcproject.api.model.Game;
 
 @Service("gameService")
 public class GameServiceImpl implements GameService {
